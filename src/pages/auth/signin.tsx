066d55d75e1429103553ef85f8c06e06a51b45a9
ratnadeep-dentsu <ratnadeep.bhattacharyya@dentsu.com>
2022-12-23 03:54:32 +0530

    remove

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -22,7 +22,6 @@ const SignIn: NextPage<Props> = ({ providers, csrfToken }) => {
         <div className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
-              <br/>{csrfToken}
               {provider.name !== "Email" ? (
                 <button
                   className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
